16309d077eecbc1f4e31cc3a9449a344fcf2a2ae
Nikhil Sharma <nikhilsharma26500@outlook.com>
2024-01-21 23:11:57 +0530

    defined sample user values

--- server.ts
+++ server.ts
@@ -1,21 +1,43 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-export const app = express()
+export const app = express();
 
 app.user(bodyParser({}));
 
 interface Balances {
-    [key: string]: number;
+  [key: string]: number;
 }
 
 interface User {
-    id: string,
-    balance: Balances
+  id: string;
+  balance: Balances;
 }
 
 interface Order {
-    userID: string,
-    price: number,
-    quantity: number
+  userID: string;
+  price: number;
+  quantity: number;
 }
+
+export const TICKER = "GOOGLE";
+
+const users: User[] = [
+  {
+    id: "1",
+    balance: {
+      GOOGLE: 10,
+      USD: 50000,
+    },
+  },
+  {
+    id: "2",
+    balance: {
+      GOOGLE: 10,
+      USD: 50000,
+    },
+  },
+];
+
+const bids: Order[] = [];
+const asks: Order[] = [];
